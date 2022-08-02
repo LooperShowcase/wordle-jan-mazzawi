a93b237961ff3447f8dbe67565cd62617971d320
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2022-08-02 15:44:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
+const { request, response } = require("express");
 const express = require("express");
 
 const server = express();
+const port = process.env.PORT || 3000
 
-server.get("/name/:name", (request, response) => {
-  response.send("Helloo " + request.params.name);
+server.get("/guess/:word", (request, response) => {
+  const userWord = request.params.word;
+  const arr = [];
+  for (let i = 0; i < userWord.length; i++) {
+    const ch = userWord[i];
+    if (ch == theAnswer[i]) {
+      arr.push(1);
+    } else if (theAnswer.includes(ch)) {
+      arr.push(0);
+    } else {
+      arr.push(-1);
+    }
+  }
+  response.json(arr);
 });
-
-server.get("/", (request, response) => {
-  response.send("Helloo");
-});
-
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log("server is running on port 3000");
 });
